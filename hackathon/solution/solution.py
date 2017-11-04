03b3b61b3aa9e99323f6a26e7cefe2837de21fc0
Novica <novicasarenac@gmail.com>
2017-11-04 19:16:14 +0100

    Grid on situation and little optimization when it is off

--- hackathon/solution/solution.py
+++ hackathon/solution/solution.py
@@ -10,6 +10,11 @@ def worker(msg: DataMessage) -> ResultsMessage:
     """TODO: This function should be implemented by contestants."""
     # Details about DataMessage and ResultsMessage objects can be found in /utils/utils.py
 
+    if msg.grid_status == True:
+        return gridOn(msg)
+    else:
+        return gridOff(msg)
+
     # Dummy result is returned in every cycle here
     return ResultsMessage(data_msg=msg,
                           load_one=True,
@@ -17,6 +22,46 @@ def worker(msg: DataMessage) -> ResultsMessage:
                           load_three=True,
                           power_reference=0.0,
                           pv_mode=PVMode.ON)
+
+def gridOn(msg: DataMessage) -> ResultsMessage:
+    if msg.bessSOC < 0.8:
+        return ResultsMessage(data_msg=msg,
+                              load_one=True,
+                              load_two=True,
+                              load_three=True,
+                              power_reference=-0.1,
+                              pv_mode=PVMode.ON)
+    else:
+        return ResultsMessage(data_msg=msg,
+                              load_one=True,
+                              load_two=True,
+                              load_three=True,
+                              power_reference=0.0,
+                              pv_mode=PVMode.ON)
+
+def gridOff(msg: DataMessage) -> ResultsMessage:
+    if msg.current_load <= msg.solar_production:
+        return ResultsMessage(data_msg=msg,
+                              load_one=True,
+                              load_two=True,
+                              load_three=True,
+                              power_reference=0.0,
+                              pv_mode=PVMode.ON)
+    elif msg.current_load <= msg.solar_production + 6:
+        return ResultsMessage(data_msg=msg,
+                              load_one=True,
+                              load_two=True,
+                              load_three=True,
+                              power_reference=msg.current_load - msg.solar_production,
+                              pv_mode=PVMode.ON)
+    else:
+        return ResultsMessage(data_msg=msg,
+                              load_one=True,
+                              load_two=False,
+                              load_three=False,
+                              power_reference=6.0,
+                              pv_mode=PVMode.ON)
+
 
 
 def run(args) -> None:
